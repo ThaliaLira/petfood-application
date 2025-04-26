f38985e3687689858a6da5cd9d024cf4b9b6d600
IgorMBra <dlaytwo@gmail.com>
2025-04-26 12:58:35 -0300

    Update Login.js

--- Login.js
+++ Login.js
@@ -11,7 +11,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     if (!username || !senha) {
-      alert("Erro", "Por favor, preencha todos os campos.");
+      Alert.alert("Erro", "Por favor, preencha todos os campos.");
       return;
     }
 
